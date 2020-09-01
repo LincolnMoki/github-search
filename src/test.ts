3de2602cd80abcca01dde7c0e93dff8b29f02fd6
Ronny <mwithiamoki@gmail.com>
2020-09-01 09:40:11 +0300

    initial commit

--- src/test.ts
+++ src/test.ts
@@ -6,18 +6,15 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
+declare const require: any;
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
+
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
