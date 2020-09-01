b140aed022befc3b19a43ea872cc17dc5affc104
Ronny <mwithiamoki@gmail.com>
2020-09-01 09:54:46 +0300

    user

---
+++ src/app/user.ts
@@ -0,0 +1,11 @@
+export class user {
+    public username: string;
+    public html_url: string;
+    public location: string;
+    public followers: number;
+    public following: number;
+    public avatar_url: string;
+    public created_at: Date;
+
+
+}
