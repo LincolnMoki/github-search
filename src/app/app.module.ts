cc905e5819f344620af923b7064728a0d239e5a3
Ronny <mwithiamoki@gmail.com>
2020-09-01 09:53:56 +0300

    module ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+app module .ts
+
+
+
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule,NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { ROUTES, RouterModule, Route, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GithubService} from './github.service';
+import { ProfileService} from './profile.service'
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { from } from 'rxjs';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ProfileComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA]
+})
+export class AppModule { }
